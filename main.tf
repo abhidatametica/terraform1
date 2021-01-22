0f133edf8ce81363b344fe6278d709c10e3c52a5
abhidatametica <75563198+abhidatametica@users.noreply.github.com>
2021-01-22 16:30:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
+provider "google" {
+    project = "abhisheks-demo"
+  
+}
 module "cloud_sql" {
     source = "./cloud_sql/"
     project_name = var.project_name
+    network_name = var.network_name
     sql_instance_name= "datametica-demo-sql-vm"
     disk_size = "10"
     sql_machine_type = "db-n1-standard-1"
     database_version = "MYSQL_5_7"
     region = var.region 
-    zone = var.zone
     availability_type =var.availability_type
     
 
