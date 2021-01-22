b47c963a9124684a618b1ccd6593d4ca8124f754
abhidatametica <75563198+abhidatametica@users.noreply.github.com>
2021-01-22 16:30:59 +0530

    Update variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "project name" {}
+
+variable "region " {
+    default = "us-central1"
+}
+
+variable "zone" {
+    default = "us-central1-a"
+}
+
+variable "availability_type" {
+    default = "ZONAL"
+}
+
+variable "composer_machine_type"{
+ default = "n1-standard-1"
+}
