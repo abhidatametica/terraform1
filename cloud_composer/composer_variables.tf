7def330519379ef6cffc2cb0675854a01e4b129f
abhidatametica <75563198+abhidatametica@users.noreply.github.com>
2021-01-28 18:22:40 +0530

    Update composer_variables.tf

--- cloud_composer/composer_variables.tf
+++ cloud_composer/composer_variables.tf
@@ -1,7 +1,13 @@
 variable "project_name"{}
+variable "composer_name"{}
 variable "region"{}
 variable "zone"{}
 variable "subnet"{}
 variable "service_account"{}
 variable "network_name"{}
-variable "composer_mchine_type"{}
+variable "composer_machine_type"{}
+variable "network_tag"{}
+variable "image"{
+    default = "composer-1.14.0-airflow-1.10.12"
+}
+
