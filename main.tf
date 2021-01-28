91170a438add4a977f37878b3566016bc53022c0
abhidatametica <75563198+abhidatametica@users.noreply.github.com>
2021-01-28 18:27:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "google" {
     project = "abhisheks-demo"
-  
+    credentials = "abhishek-key.json"
 }
 module "cloud_sql" {
     source = "./cloud_sql/"
@@ -10,10 +10,34 @@ module "cloud_sql" {
     disk_size = "10"
     sql_machine_type = "db-n1-standard-1"
     database_version = "MYSQL_5_7"
-    region = var.region 
+    region = var.region
    # availability_type =var.availability_type
-    
 
 
-    
+
+
+}
+
+
+
+module "custom_role" {
+    source = "./iam/custom_roles"
+    project_name =var.project_name
+
+}
+
+module "cloud_composer" {
+    source = "./cloud_composer/"
+    project_name=var.project_name
+    composer_name="composer-01"
+    region = "us-central1"
+    zone = "us-central1-a"
+    network_name = var.network_name
+    subnet =var.subnet
+    service_account="98370415066-compute@developer.gserviceaccount.com"
+    #image="composer-1.14.0-airflow-1.10.12"
+    composer_machine_type =var.composer_machine_type
+    #python = "3"
+    network_tag=var.network_tag
+
 }
