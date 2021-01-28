ac3cee380b94f1d4c29585cf31d7ee737ada27e0
abhidatametica <75563198+abhidatametica@users.noreply.github.com>
2021-01-28 18:21:58 +0530

    Update composer_main.tf

--- cloud_composer/composer_main.tf
+++ cloud_composer/composer_main.tf
@@ -1,20 +1,28 @@
-#create cloud composer 
+
+#create cloud composer
 resource "google_composer_environment" "cloud_composer1" {
-name = "cloud_composer1"
+name = var.composer_name
 project = var.project_name
 region = var.region
 
 config {
     node_count = 3
+
     node_config {
       zone = var.zone
       machine_type = var.composer_machine_type
       disk_size_gb ="100"
       network = var.network_name
-      subnetwork =var.subnet 
-      service_account = var.service_account 
+      subnetwork =var.subnet
+      service_account = var.service_account
+      tags = var.network_tag
     }
+
+    software_config {
+      image_version = var.image
+      python_version = "3"
+
+    }
+ }
 }
-     
-  
-}
+
