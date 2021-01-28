b3f3410145840f98efb147e879d2481c6733387b
abhidatametica <75563198+abhidatametica@users.noreply.github.com>
2021-01-28 18:19:31 +0530

    Update cloud_sql_setup.tf

--- cloud_sql/cloud_sql_setup.tf
+++ cloud_sql/cloud_sql_setup.tf
@@ -1,7 +1,7 @@
 #create a private ip #
 
-resource "google_compute_global_address" "default" {
-  name = "private_network1"
+resource "google_compute_global_address" "private_network1" {
+  name = "private-network1"
   project = var.project_name
   address_type = "INTERNAL"
   purpose = "VPC_PEERING"
@@ -16,19 +16,20 @@ resource "google_compute_global_address" "default" {
 resource "google_sql_database_instance" "cloud_sql_instance1" {
     name = var.sql_instance_name
     region = var.region
+    project = var.project_name
     database_version = var.database_version
-    
+
     settings {
       tier =var.sql_machine_type   #which machine type we want  eg postgrss or mysql
       disk_size = var.disk_size
 
       ip_configuration {
-        ipv4_enabled = false
+        ipv4_enabled = "false"
         private_network = var.network_name
-             
+
        }
 
 
     }
-  
+
 }
